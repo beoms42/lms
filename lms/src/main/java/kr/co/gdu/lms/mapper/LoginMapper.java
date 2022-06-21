966921be91914aa3403b4e44ff1f4ad8c1da6b8e
Parkhw317 <sukisimmax@gmail.com>
2022-06-21 19:57:29 +0900

    [박희원] 1) LoginMapper 수정

--- lms/src/main/java/kr/co/gdu/lms/mapper/LoginMapper.java
+++ lms/src/main/java/kr/co/gdu/lms/mapper/LoginMapper.java
@@ -3,10 +3,22 @@ package kr.co.gdu.lms.mapper;
 import org.apache.ibatis.annotations.Mapper;
 
 import kr.co.gdu.lms.vo.Login;
+import kr.co.gdu.lms.vo.Manager;
+import kr.co.gdu.lms.vo.Student;
+import kr.co.gdu.lms.vo.Teacher;
 
 @Mapper
 public interface LoginMapper {
 
+	// 학생 아이디 찾기
+	Student selectStudentLoginId(Student student);
+	
+	// 매니저 아이디 찾기
+	Manager selectManagerLoginId(Manager manager);
+	
+	// 강사 아이디 찾기
+	Teacher selectTeacherLoginId(Teacher teacher);
+	
 	// 로그인 아이디와 비밀번호 확인하고 맞다면 해당 level 출력 
 	Login loginAndSelectLevel(Login loginTest);
 }
