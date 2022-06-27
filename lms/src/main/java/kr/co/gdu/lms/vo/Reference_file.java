93b6c989e169d7c9a6251dd33807796b9d3d3741
hanji55 <hanji55@DESKTOP-227SEBC>
2022-06-27 10:10:52 +0900

    [한정일] vo. ReferenceFile 생성

---
+++ lms/src/main/java/kr/co/gdu/lms/vo/Reference_file.java
@@ -0,0 +1,10 @@
+package kr.co.gdu.lms.vo;
+
+public class Reference_file {
+	private int referenceFileNo;
+	private String referenceFileName;
+	private String referenceFileOrgin;
+	private String referenceFileType;
+	private int referenceFileSzie;
+	private String createDate;
+}
