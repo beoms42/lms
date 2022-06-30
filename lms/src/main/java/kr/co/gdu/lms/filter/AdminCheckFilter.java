30c32f5c5525e29b7f6c542a5c62306fe5d6bbdb
rutong2 <rutong230@naver.com>
2022-06-30 14:51:25 +0900

    [이찬희] 1) AdminCheckFilter 생성 > 총관리자가 아니면 메인페이지로 이동 시키는 필터 생성

---
+++ lms/src/main/java/kr/co/gdu/lms/filter/AdminCheckFilter.java
@@ -0,0 +1,41 @@
+package kr.co.gdu.lms.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.co.gdu.lms.log.CF;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@WebFilter("/adminCheck/*")
+public class AdminCheckFilter extends HttpFilter implements Filter {
+       
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		if(request instanceof HttpServletRequest) {
+			HttpSession session = ((HttpServletRequest)request).getSession();
+			int level = (int)session.getAttribute("sessionLv");
+			log.debug(CF.LCH + "AdminCheckFilter.doFilter level : " + level + CF.RS);
+			
+			if(level != 4) {
+				if(response instanceof HttpServletResponse) {
+					log.debug(CF.LCH + "총 관리자 제외 접근 불가" + CF.RS);
+					((HttpServletResponse)response).sendRedirect(((HttpServletRequest)request).getContextPath()+"/loginCheck/main");
+					return;
+				}
+			}
+		}
+		chain.doFilter(request, response);
+	}
+
+
+}
