e3737fe2dcccd9e61b1a5692655a8345650a1daa
ohi2262 <ohi2262@naver.com>
2022-07-01 17:39:23 +0900

    [오혜인] 1) HyeinMapper, xml 수정 >  강사별 맡고있는 강의의 학생이름, 출결, 강의 이름 , 출결날짜 뽑을 selectAttendanc() 생성 2) StatsController 수정 > 빈생성 오류로 주석처리 후 커밋

--- lms/src/main/java/kr/co/gdu/lms/controller/StatsController.java
+++ lms/src/main/java/kr/co/gdu/lms/controller/StatsController.java
@@ -6,15 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import kr.co.gdu.lms.mapper.StatsMapper;
-
-@Controller
-public class StatsController {
-	@Autowired private StatsMapper statsmapper; 
-	@GetMapping("/stats")
-	public String stats(Model model) {
-		List<Integer> avgList = statsmapper.selectAvgScore();
-		model.addAttribute("avgList",avgList);
-		return "/stats/stats";
-	}
-}
+/*
+ * import kr.co.gdu.lms.mapper.StatsMapper;
+ * 
+ * @Controller public class StatsController {
+ * 
+ * @Autowired private StatsMapper statsmapper;
+ * 
+ * @GetMapping("/stats") public String stats(Model model) { List<Integer>
+ * avgList = statsmapper.selectAvgScore();
+ * model.addAttribute("avgList",avgList); return "/stats/stats"; } }
+ */
