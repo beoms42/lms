bc1c6c1018724147425f01fc3ee9069d7f8e8d22
HSK <HSK@DESKTOP-V56O9R4>
2022-07-11 13:35:01 +0900

    [강민창] 통계.jsp 하나로 통합

--- lms/src/main/java/kr/co/gdu/lms/controller/StatsController.java
+++ lms/src/main/java/kr/co/gdu/lms/controller/StatsController.java
@@ -5,32 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StatsController {
-	@GetMapping("/loginCheck/gradeStats")
+	@GetMapping("/loginCheck/stats")
 	public String addGrade() {
-		return "stats/gradeStats";
+		return "stats/stats";
 	}
-	@GetMapping("/loginCheck/dropRecordStats")
-	public String addDropRecords() {
-		return "stats/dropRecordStats";
-	}
-	@GetMapping("/loginCheck/stats_main")
-	public String addStatsMain() {
-		return "stats/stats_main";
-	}
-	@GetMapping("/loginCheck/ageAvgStats")
-	public String addAgeAvg() {
-		return "stats/classAgeAvgStats";
-	}
-	@GetMapping("/loginCheck/genderStats")
-	public String addGenderStats() {
-		return "stats/genderRateStats";
-	}
-	@GetMapping("/loginCheck/perClass")
-	public String addPerClass() {
-		return "stats/perClassStatas";
-	}
-	@GetMapping("/loginCheck/graduate")
-	public String addGraduate() {
-		return "/stats/graduateStats";
-	}
+
 }
